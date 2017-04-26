cc2c99a25105daefb4dc698e1becf90937e2aeca
lixiao_fly@sina.cn <lixiao_fly@sina.cn>
2017-04-26 13:43:47 +0800

    Signed-off-by: lixiao_fly@sina.cn <lixiao_fly@sina.cn>

--- watchdir.sh
+++ watchdir.sh
@@ -4,6 +4,7 @@ src_path=/mnt/ftp/DPI
 dst_path=/mnt/DPI/XDR
 log_path=/tmp/DPI/log
 temp_path=/tmp/DPI
+#log_file=$log_path/decompressed.log
 
 create_dir() {
 	if [ ! -d "${src_path}" ]; then 
@@ -17,10 +18,10 @@ main() {
 	$(create_dir)
  	inotifywait -mrq --timefmt '%d-%m-%y-%H:%M:%S' --format '%T %w %f' -e close_write $src_path | while read  time dir file event
 	do
-		echo "${time}_${dir}_${file}" >> $log_path/decompressed.log 
+		echo "${time}_${dir}_${file}" >> ${log_file} 
 	
 		if [[ "${file##*.}" = "z4" ]]; then
-			dstfile = "${file%.*}.xdr"
+			dstfile="${file%.*}.xdr"
 			lz4 -f -d "${src_path}/$file"  "${temp_path}/${dstfile}"
 			mv "${temp_path}/${dstfile}"   "${dst_path}/${dstfile}"
 		fi 
